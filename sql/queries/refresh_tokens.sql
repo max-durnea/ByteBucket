06e2d205ad15c16e7469e6658d490b27672e9f59
max-durnea <maxim.durnea@mail.ru>
2025-10-08 23:02:59 +0300

    Add refresh_token functions

---
+++ sql/queries/refresh_tokens.sql
@@ -0,0 +1,11 @@
+-- name: CreateRefreshToken :one
+
+INSERT INTO refresh_tokens(token,created_at,updated_at,user_id,expires_at,revoked_at)
+VALUES($1,$2,$3,$4,$5,$6)
+RETURNING *;
+-- name: GetUserFromRefreshToken :one
+
+SELECT users.*
+FROM refresh_tokens JOIN users ON refresh_tokens.user_id = users.id
+WHERE refresh_tokens.token = $1;
+
