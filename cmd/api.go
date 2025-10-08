0b8fb8a001414dc82317d6888ec316379e223f8c
max-durnea <maxim.durnea@mail.ru>
2025-10-08 15:36:04 +0300

    Add apiConfig struct and open database

---
+++ cmd/api.go
@@ -0,0 +1,8 @@
+
+import (
+	"github.com/max-durnea/ByteBucket/internal/database"
+	"fmt"
+)
+type apiConfig struct{
+	db *database.Queries
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    _ "github.com/lib/pq"
+    "database/sql"
+    "github.com/max-durnea/ByteBucket/internal/database"
+    "fmt"
+    "net/http"
+    "github.com/joho/godotenv"
+    "os"
+)
+
+var apiCfg = apiConfig{}
 
 func main() {
     fmt.Println("Hello, ByteBucket!")
+    godotenv.Load()
+    dbURL := os.Getenv("DATABASE_URL")
+    db, err := sql.Open("postgres",dbURL)
+    if err != nil {
+        fmt.Printf("ERROR: Could not open database: %v",err)
+        os.Exit(1)
+    }
+    dbQueries := database.New(db)
+    apiCfg.db = dbQueries
+
+    mux := http.NewServeMux()
+    server := &http.Server{}
+    server.Handler = mux
+    
 }
