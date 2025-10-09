2449642101a19d72572480edcbf54f5802c5a395
max-durnea <maxim.durnea@mail.ru>
2025-10-09 14:19:42 +0300

    Format

--- cmd/refreshTokenHandler.go
+++ cmd/refreshTokenHandler.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
-	"time"
 	"github.com/max-durnea/ByteBucket/internal/auth"
+	"net/http"
+	"time"
 )
 
 func (cfg *apiConfig) refreshTokenHandler(w http.ResponseWriter, r *http.Request) {
-	type params struct{
+	type params struct {
 		Token string `json:"refresh_token"`
 	}
 
@@ -16,36 +16,36 @@ func (cfg *apiConfig) refreshTokenHandler(w http.ResponseWriter, r *http.Request
 	var data params
 	err := decoder.Decode(&data)
 	if err != nil {
-		respondWithError(w,400,"Could not decode refresh token")
+		respondWithError(w, 400, "Could not decode refresh token")
 		return
 	}
-	tokenDB,err:=cfg.db.GetRefreshToken(r.Context(),data.Token)
+	tokenDB, err := cfg.db.GetRefreshToken(r.Context(), data.Token)
 	if err != nil {
-		respondWithError(w,400,"Bad Token")
+		respondWithError(w, 400, "Bad Token")
 		return
 	}
 	if time.Now().After(tokenDB.ExpiresAt) {
-		respondWithError(w,400,"Token Expired")
+		respondWithError(w, 400, "Token Expired")
 		return
 	}
 	if tokenDB.RevokedAt.Valid {
-		respondWithError(w,400,"Token revoked")
+		respondWithError(w, 400, "Token revoked")
 		return
 	}
 	//If refresh token is valid, extract the user_id from it and create a jwt_token based on this
-	userDB,err:= cfg.db.GetUserFromRefreshToken(r.Context(),data.Token)
+	userDB, err := cfg.db.GetUserFromRefreshToken(r.Context(), data.Token)
 	if err != nil {
-		respondWithError(w,400,"User does not exist")
+		respondWithError(w, 400, "User does not exist")
 		return
 	}
 	//create the jwt
-	jwt_token,err:=auth.MakeJWT(userDB.ID,cfg.tokenSecret,15*time.Minute)
+	jwt_token, err := auth.MakeJWT(userDB.ID, cfg.tokenSecret, 15*time.Minute)
 	if err != nil {
-		respondWithError(w,400,"Could not generate JWT token")
+		respondWithError(w, 400, "Could not generate JWT token")
 		return
 	}
 	respondWithJson(w, 200, struct {
-    AccessToken string `json:"access_token"`
+		AccessToken string `json:"access_token"`
 	}{
 		AccessToken: jwt_token,
 	})
